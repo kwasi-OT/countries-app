76195f0831ba2a3c87d1ae58a5e0ea645c9fd5af
Kwasi O-T <kwasi@kudigo.com>
2023-11-06 22:15:56 +0000

    added content to Navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { HiOutlineMoon } from 'react-icons/hi';
 
 const Navigation = () => {
     return (
         <div>
-        
+            <div className='d-flex justify-content-between'>
+                <div>
+                    <h3>Where in the world?</h3>
+                </div>
+                <div className='d-flex align-items-center '>
+                    <HiOutlineMoon/>
+                    <p>Dark Mode</p>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Navigation
+export default Navigation;
