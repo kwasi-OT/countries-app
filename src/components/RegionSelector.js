3daca31d6a7f0729afeed1ea282786df10a8d736
Kwasi O-T <kwasi@kudigo.com>
2023-11-07 23:33:07 +0000

    created a selection input form

--- src/components/RegionSelector.js
+++ src/components/RegionSelector.js
@@ -3,7 +3,13 @@ import React from 'react'
 const RegionSelector = () => {
     return (
         <div>
-        
+            <select value='Filter By Region'>
+                <option>Africa</option>
+                <option>America</option>
+                <option>Asia</option>
+                <option>Europe</option>
+                <option>Oceania</option>
+            </select>
         </div>
     )
 }
