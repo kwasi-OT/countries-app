81dbc12ce1d4358148a99ef296032807f24c73af
Kwasi O-T <kwasi@kudigo.com>
2023-11-07 23:18:51 +0000

    increased width of input field

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,7 @@ import { AiOutlineSearch } from 'react-icons/ai';
 const SearchForm = () => {
     return (
         <div className='searchForm d-flex align-items-center gap-3 mt-4'>
-            <AiOutlineSearch size='1rem' color='#B2B2B2'/>
+            <AiOutlineSearch size='1rem' color='#B2B2B2' className='searchIcon'/>
             <input type='text' placeholder='Search for a country…'/>
         </div>
     )
