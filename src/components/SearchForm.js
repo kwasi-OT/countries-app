8993cf86513c778af16e17a6d723b8aed19a99b9
Kwasi O-T <kwasi@kudigo.com>
2023-11-07 22:42:46 +0000

    imported search icon

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const SearchForm = () => {
     return (
         <div>
-        
+            <AiOutlineSearch />
+            <input type='text' placeholder='Search for a country…'/>
         </div>
     )
 }
