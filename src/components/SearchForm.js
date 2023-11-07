48430929ab95df2333ba858d6ea6a8258b248862
Kwasi O-T <kwasi@kudigo.com>
2023-11-07 23:06:56 +0000

    added input and placeholder styling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,7 +3,7 @@ import { AiOutlineSearch } from 'react-icons/ai';
 
 const SearchForm = () => {
     return (
-        <div className='searchForm d-flex align-item-center mt-4'>
+        <div className='searchForm d-flex justify-content-center align-items-center gap-3 mt-4'>
             <AiOutlineSearch size='1rem' color='#B2B2B2'/>
             <input type='text' placeholder='Search for a country…'/>
         </div>
