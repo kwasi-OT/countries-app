ab349236c3ed0b0bc0432b53977fa7042528a67a
Kwasi O-T <kwasi@kudigo.com>
2023-11-07 22:58:34 +0000

    added styling to search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,8 +3,8 @@ import { AiOutlineSearch } from 'react-icons/ai';
 
 const SearchForm = () => {
     return (
-        <div className='d-flex'>
-            <AiOutlineSearch />
+        <div className='searchForm d-flex align-item-center mt-4'>
+            <AiOutlineSearch size='1rem' color='#B2B2B2'/>
             <input type='text' placeholder='Search for a country…'/>
         </div>
     )
