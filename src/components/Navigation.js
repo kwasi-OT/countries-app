3aad9dd573b7767f1079ac5f7993944a970d36c9
Kwasi O-T <kwasi@kudigo.com>
2023-11-07 22:34:47 +0000

    added top margin to toggle section

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = () => {
                 <div>
                     <h3 className='title'>Where in the world?</h3>
                 </div>
-                <div className='d-flex gap-2 align-items-center '>
+                <div className='d-flex gap-2 mt-2'>
                     <HiOutlineMoon size='1rem' color='#111517' className='mb-3'/>
                     <p className='toggleText'>Dark Mode</p>
                 </div>
