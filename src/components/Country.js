f957ca8ded686369d51e769bee97fd5dca75c950
Kwasi O-T <kwasi@kudigo.com>
2023-11-08 11:35:44 +0000

    imported country flags into country component

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Germany } from '../assets/Germany.png';
+import { America } from '../assets/America.png';
+import { Brazil } from '../assets/Brazil.png';
+import { Albania } from '../assets/Albania.png';
+import { Iceland } from '../assets/Iceland.png';
+import { Aland } from '../assets/Aland Island.png';
+import { Algeria } from '../assets/Algeria.png';
+import { Afghanistan } from '../assets/Afghanistan.png';
 
 const Country = () => {
     return (
         <div>
-        
+            <div></div>
         </div>
     )
 }
 
-export default Country
+export default Country;
