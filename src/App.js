5f3a82f950317cfa55863fc6da6ee2b5b8d6ed0b
Kwasi O-T <kwasi@kudigo.com>
2023-12-28 22:31:15 +0000

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
 import Countries from './components/Countries';
+import Country from './components/Country';
 import Navigation from './components/Navigation';
 import RegionSelector from './components/RegionSelector';
 import SearchForm from './components/SearchForm';
+import { BrowserRouter, Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app'>
-      <Navigation />
-      <div className='appContainer'>
-        <div className='searchGroup'>
-          <SearchForm />
-          <RegionSelector />
+    <Router>
+      <div className='app'>
+        <Navigation />
+      <Route path='/'>
+        <div className='appContainer'>
+          <div className='searchGroup'>
+            <SearchForm />
+            <RegionSelector />
+          </div>
+          <Countries />
         </div>
-        <Countries />
+      </Route>
+      <Route path='/countries/:name' children={<Country />}></Route>
       </div>
-    </div>
+    </Router>
   );
 }
 
