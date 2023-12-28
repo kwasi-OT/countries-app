677b54ed9d5159403d51225171ba268fd35b5cc8
Kwasi O-T <kwasi@kudigo.com>
2023-12-28 03:16:54 +0000

    added map method to iterate through data

--- src/components/Country.js
+++ src/components/Country.js
@@ -30,94 +30,30 @@ const Country = () => {
 
     return (
         <div className='countryList mt-4'>
-            <div className='countryCard'>
-                <img src='../assets/Germany.png' alt='German flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>Germany</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 81,770,900</p>
-                        <p><b>Region:</b> Europe</p>
-                        <p><b>Capital:</b> Berlin</p>
+            {countries.map((country) => {
+                const {
+                    ccn3,
+                    name,
+                    capital,
+                    population,
+                    flags,
+                    region,
+                } = country
+
+                return (
+                    <div key={ccn3} className='countryCard'>
+                        <img src={flags.svg} alt='{name.common} flag'/>
+                        <div className='countryText'>
+                            <h3 className='countryName'>{name.common}</h3>
+                            <div className='countryMeta'>
+                                <p><b>Population:</b> {population}</p>
+                                <p><b>Region:</b> {region}</p>
+                                <p><b>Capital:</b> {capital}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className='countryCard'>
-                <img src='../assets/America.png' alt='American flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>United States of America</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 323,947,000</p>
-                        <p><b>Region:</b> Americas</p>
-                        <p><b>Capital:</b> Washington, D.C.</p>
-                    </div>
-                </div>
-            </div>
-            <div className='countryCard'>
-                <img src='../assets/Brazil.png' alt='Brazilian flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>Brazil</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 206,135,893</p>
-                        <p><b>Region:</b> Americas</p>
-                        <p><b>Capital:</b> Brasilia</p>
-                    </div>
-                </div>
-            </div>
-            <div className='countryCard'>
-                <img src='../assets/Iceland.png' alt='Iceland flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>Iceland</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 334,300</p>
-                        <p><b>Region:</b> Europe</p>
-                        <p><b>Capital:</b> Reykjavik</p>
-                    </div>
-                </div>
-            </div>
-            <div className='countryCard'>
-                <img src='../assets/Afghanistan.png' alt='Afghan flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>Afghanistan</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 27,657,145</p>
-                        <p><b>Region:</b> Asia</p>
-                        <p><b>Capital:</b> Kabul</p>
-                    </div>
-                </div>
-            </div>
-            <div className='countryCard'>
-                <img src='../assets/Aland Island.png' alt='Aland Islands flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>Aland Islands</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 28,875</p>
-                        <p><b>Region:</b> Europe</p>
-                        <p><b>Capital:</b> Mariehamn</p>
-                    </div>
-                </div>
-            </div>
-            <div className='countryCard'>
-                <img src='../assets/Albania.png' alt='Albanian flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>Albania</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 2,886,026</p>
-                        <p><b>Region:</b> Europe</p>
-                        <p><b>Capital:</b> Tirana</p>
-                    </div>
-                </div>
-            </div>
-            <div className='countryCard'>
-                <img src='../assets/Algeria.png' alt='Algerian flag'/>
-                <div className='countryText'>
-                    <h3 className='countryName'>Algeria</h3>
-                    <div className='countryMeta'>
-                        <p><b>Population:</b> 40,400,000</p>
-                        <p><b>Region:</b> Africa</p>
-                        <p><b>Capital:</b> Algiers</p>
-                    </div>
-                </div>
-            </div>
+                )
+            } )}
         </div>
     )
 }
