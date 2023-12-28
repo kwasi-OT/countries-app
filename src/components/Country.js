77a0ecc02e2463bac2b11a1c2ea6d0cd54f297f0
Kwasi O-T <kwasi@kudigo.com>
2023-12-28 03:34:57 +0000

    added conditional rendering and error handling

--- src/components/Country.js
+++ src/components/Country.js
@@ -28,34 +28,60 @@ const Country = () => {
         getCountry();
     }, [])
 
-    return (
-        <div className='countryList mt-4'>
-            {countries.map((country) => {
-                const {
-                    ccn3,
-                    name,
-                    capital,
-                    population,
-                    flags,
-                    region,
-                } = country
+    if (loading) {
+        return(
+            <div className='d-flex flex-column justify-content-center align-items-center mt-10'>
+                <h1 className='loading'>Loading...</h1>
+            </div>
+        )
+    }
 
-                return (
-                    <div key={ccn3} className='countryCard'>
-                        <img src={flags.svg} alt='{name.common} flag'/>
-                        <div className='countryText'>
-                            <h3 className='countryName'>{name.common}</h3>
-                            <div className='countryMeta'>
-                                <p><b>Population:</b> {population}</p>
-                                <p><b>Region:</b> {region}</p>
-                                <p><b>Capital:</b> {capital}</p>
+    if (!countries) {
+        return(
+            <div>
+                {error &&
+                <div className='errorMessage d-flex flex-column justify-content-center align-items-center'>
+                    <div className='imoji'>😕</div>
+                    <div><h2>No Data Found</h2></div>
+                    <div>
+                        <p>
+                            Sorry pal, we couldn't load the data for the country/countries you're looking for. Please try again!!
+                        </p>
+                    </div>
+                </div>
+                }
+            </div>
+        )
+    } else {
+        return (
+            <div className='countryList mt-4'>
+                {countries.map((country) => {
+                    const {
+                        ccn3,
+                        name,
+                        capital,
+                        population,
+                        flags,
+                        region,
+                    } = country
+    
+                    return (
+                        <div key={ccn3} className='countryCard'>
+                            <img src={flags.svg} alt='{name.common} flag'/>
+                            <div className='countryText'>
+                                <h3 className='countryName'>{name.common}</h3>
+                                <div className='countryMeta'>
+                                    <p><b>Population:</b> {population}</p>
+                                    <p><b>Region:</b> {region}</p>
+                                    <p><b>Capital:</b> {capital}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            } )}
-        </div>
-    )
+                    )
+                } )}
+            </div>
+        )
+    }
 }
 
 export default Country;
