6c888410fed9d7745d1ff18a93aa8a3150965eab
Kwasi O-T <kwasi@kudigo.com>
2023-12-29 00:50:38 +0000

    added logic to Home and cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import Countries from './components/Countries';
 import Country from './components/Country';
 import Header from './components/Header';
-import RegionSelector from './components/RegionSelector';
-import SearchForm from './components/SearchForm';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className='app'>
+    <Router className='app'>
         <Header />
-      <Route path='/'>
-        <div className='appContainer'>
-          <div className='searchGroup'>
-            <SearchForm />
-            <RegionSelector />
-          </div>
-          <Countries />
-        </div>
-      </Route>
-      <Route path='/countries/:name' children={<Country />}></Route>
-      </div>
+      <Routes>
+        <Route path='/'></Route>
+        <Route path='/countries/:name' children={<Country />}></Route>
+      </Routes>
     </Router>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import SearchForm from './SearchForm'
+import RegionSelector from './RegionSelector'
+import Countries from './Countries'
 
 const Home = () => {
     return (
-        <div>
-        
+        <div className='app'>
+            <div className='appContainer'>
+                <div className='searchGroup'>
+                    <SearchForm />
+                    <RegionSelector />
+                </div>
+                <Countries />
+            </div>
         </div>
     )
 }
