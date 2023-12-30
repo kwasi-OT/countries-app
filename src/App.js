df40e7eaf8a5cdb8b2c10228e8b29a0474beed41
Kwasi O-T <kwasi@kudigo.com>
2023-12-30 17:36:37 +0000

    implemented dark theming in app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Country from './components/Country';
 import Header from './components/Header';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import { ThemeContext } from './components/ContextTheme';
+import { useContext } from 'react';
 
 function App() {
+  const{darkTheme} = useContext(ThemeContext);
   return (
-    <Router className='app'>
+    <Router className={`app ${darkTheme? 'dark' : ''}`}>
         <Header />
       <Routes>
         <Route exact path='/' element={<Home/>} />
